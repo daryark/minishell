1743f22f8e5d192513376699541645bc56a856de
Dasha Yarkovska <dyarkovs@c2s2d5.42wolfsburg.de>
2024-05-07 21:03:56 +0200

    save

--- incl/minishell.h
+++ incl/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:21:52 by btvildia          #+#    #+#             */
-/*   Updated: 2024/05/06 20:35:46 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:55:29 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,15 @@
 // # include <dirent.h>
 // # include <fcntl.h>
 // # include <readline/history.h>
-// # include <readline/readline.h>
+# include <readline/readline.h>
 // # include <signal.h>
 // # include <sys/stat.h>
 // # include <sys/types.h>
 // # include <sys/wait.h>
 
+typedef struct s_mshell
+{
+	char	**envp;
+}			   t_mshell;
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,41 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:23:44 by btvildia          #+#    #+#             */
-/*   Updated: 2024/05/06 20:45:36 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/05/07 18:08:33 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/minishell.h"
+#include <stdio.h>
 
-int	main(int ac, char **av, char **envp)
-{
-	(void)av;
-	(void)envp;
-	if (ac != 1)
-		exit(write(1, RED "No arguments accepted!\n" RE, 32));
-	else
-		write(1, GREEN "OK\n" RE, 14);
-	return (0);
+
+void minishell_loop(void) {
+    char *input;
+
+    while (1) {
+        input = readline(YELLOW "Minishell: " RE);
+
+        if (!input || *input == '\0') {
+            // If input is NULL or empty, break out of the loop
+            printf("Exiting...\n");
+            free(input);  // Freeing if input is NULL is safe
+            continue;
+        }
+
+        add_history(input);  // Add input to history
+        printf("You entered: %s\n", input);
+        free(input);  // Free memory allocated by readline
+    }
+}
+
+int main(int ac, char **av, char **envp) {
+    (void)av;
+    (void)envp;
+    if (ac != 1)
+        exit(write(1, RED "No arguments accepted!\n" RE, 32));
+    else {
+        write(1, GREEN "OK\n" RE, 14);
+        minishell_loop();
+    }
+    return 0;
 }
