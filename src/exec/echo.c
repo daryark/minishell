b7363762dd04641a0e892edb52f904234e79a49e
Dasha Yarkovska <dyarkovs@c2r12s2.42wolfsburg.de>
2024-06-01 22:42:59 +0200

    save

--- src/exec/echo.c
+++ src/exec/echo.c
@@ -6,24 +6,38 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 18:48:21 by btvildia          #+#    #+#             */
-/*   Updated: 2024/06/01 17:11:05 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/06/01 18:19:18 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/execute.h"
 
+// static void	n_flag(t_cmdarr cmd)
+// {
+// 	//cut the echo fn
+// }
+
 void	ft_echo(t_mshell *mshell)
 {
 	int	i;
+	int	j;
 	int	n_line;
 
 	i = 1;
 	n_line = 1;
-	// while (mshell->cmdarr[mshell->cmd_num].args[i]
-	// 	&& !ft_strncmp(mshell->cmdarr[mshell->cmd_num].args[i], "-n", 3))
-	// 	i++;
-	// if (i > 1)
-	// 	n_line = 0;
+	while (mshell->cmdarr[mshell->cmd_num].args[i])
+	{
+		if (mshell->cmdarr[mshell->cmd_num].args[i][0] != '-')
+			break ;
+		j = 1;
+		while (mshell->cmdarr[mshell->cmd_num].args[i][j] == 'n')
+			j++;
+		if (mshell->cmdarr[mshell->cmd_num].args[i][j])
+			break ;
+		i++;
+	}
+	if (i > 1)
+		n_line = 0;
 	while (mshell->cmdarr[mshell->cmd_num].args[i])
 	{
 		if (mshell->cmdarr[mshell->cmd_num].args[i + 1] != NULL)
@@ -36,15 +50,3 @@ void	ft_echo(t_mshell *mshell)
 		printf("\n");
 	mshell->exit_status = 0;
 }
-
-// while (mshell->cmdarr[mshell->cmd_num].args[i])
-// 	{
-// 		if (mshell->cmdarr[mshell->cmd_num].args[i][0] != '-')
-// 			break ;
-// 		if (!ft_memchr(&mshell->cmdarr[mshell->cmd_num].args[i][1], 'n',
-// 			ft_strlen(mshell->cmdarr[mshell->cmd_num].args[i]) - 1))
-// 			break ;
-// 		i++;
-// 	}
-// 	if (i > 1)
-// 		n_line = 0;
