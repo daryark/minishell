5ab4114b831da812b7dc68812f2ad108205ab55d
Dasha Yarkovska <dyarkovs@c1r10s5.42wolfsburg.de>
2024-06-03 20:14:48 +0200

    shlvl free fixed

--- src/utils/err.c
+++ src/utils/err.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 15:30:30 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/06/03 14:56:08 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/06/03 20:11:14 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	alloc_err(void)
 
 void	ft_error_exit(char *str, char *str2, int status)
 {
-	printf("here\n");
 	write(2, "minishell: ", 11);
 	write(2, str, ft_strlen(str));
 	write(2, str2, ft_strlen(str2));
@@ -44,9 +43,80 @@ void	ft_error_return(char *str, t_mshell *mshell, int status, int modif)
 		content = "too many arguments";
 	else if (!ft_strcmp(name, "export"))
 		content = "not a valid identifier";
-	printf("minishell: %s:", name);
+	write(2, "minishell: ", 11);
 	if (*str)
-		printf(" `%s':", str);
-	printf(" %s\n", content);
+	{
+		write(2, "`", 1);
+		write(2, str, ft_strlen(str));
+		write(2, "': ", 2);
+	}
+	write(2, content, ft_strlen(content));
+	write(2, "\n", 1);
 	mshell->exit_status = status;
 }
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   err.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 15:30:30 by dyarkovs          #+#    #+#             */
+/*   Updated: 2024/06/03 19:26:08 by dyarkovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/minishell.h"
+
+void	alloc_err(void)
+{
+	exit(printf(RED "Allocation failed\n" RE));
+}
+
+void	ft_error_exit(char *str, char *str2, int status)
+{
+	write(2, "minishell: ", 11);
+	write(2, str, ft_strlen(str));
+	write(2, str2, ft_strlen(str2));
+	write(2, "\n", 1);
+	exit(status);
+}
+
+static void	ft_print_err(char *cmd, char *arg, char *content)
+{
+	write(2, "minishell: ", 11);
+	write(2, cmd, ft_strlen(cmd));
+	if (*arg)
+	{
+		write(2, "`", 1);
+		write(2, arg, ft_strlen(arg));
+		write(2, "': ", 2);
+	}
+	write(2, content, ft_strlen(content));
+	write(2, "\n", 1);
+}
+
+void	ft_error_return(char *str, t_mshell *mshell, int status, int modif)
+{
+	char	*name;
+	char	*content;
+
+	content = NULL;
+	name = mshell->cmdarr[mshell->cmd_num].args[0];
+	if (!ft_strcmp(name, "cd") && modif)
+		content = "HOME not set";
+	else if (!ft_strcmp(name, "cd") && !modif)
+		content = "No such file or directory";
+	else if (!ft_strcmp(name, "pwd"))
+		content = "error retrieving current directory";
+	else if (!ft_strcmp(name, "exit") && !modif)
+		content = "too many arguments";
+	else if (!ft_strcmp(name, "export"))
+		content = "not a valid identifier";
+	else if (!ft_strcmp(name, "unset"))
+		content = "not enough arguments";
+	ft_print_err(name, str, content);
+	mshell->exit_status = status;
+}
+
--- src/utils/err_parsing.c
+++ src/utils/err_parsing.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 23:00:05 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/06/01 16:17:16 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/06/03 20:11:51 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,12 +68,13 @@ static int	not_valid_symbols(char *s)
 
 void	syntax_err(char *s, int status)
 {
+	printf("minishell: ");
 	if (status == 2)
-		printf("Minishell: syntax error near unexpected token `%s'\n", s);
+		printf("syntax error near unexpected token `%s'\n", s);
 	if (status == 39)
-		printf("Minishell: syntax error unable to locate closing quotation\n");
-	else if (status == 4)
-		printf("minishell: export: `%s': not a valid identifier\n", s);
+		printf("syntax error unable to locate closing quotation\n");
+	if (status == 0)
+		printf("warning: shell level (1000) too high, resetting to 1\n");
 }
 
 int	input_err_check(t_mshell *mshell, char *input)
@@ -99,3 +100,4 @@ int	input_err_check(t_mshell *mshell, char *input)
 		return (0);
 	return (1);
 }
+
